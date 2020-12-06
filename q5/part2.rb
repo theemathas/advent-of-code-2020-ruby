9390ceb682cb4f0b23731150005bac3a80e0cdeb
Tim (Theemathas) Chirananthavat <theemathas@gmail.com>
2020-12-06 15:31:19 +0700

    Solve q5

---
+++ q5/part2.rb
@@ -0,0 +1,24 @@
+require 'set'
+
+lines = IO.readlines(File.join(__dir__, 'input.txt'), chomp: true)
+
+def to_id(boarding_pass)
+  raise 'Invalid pass length' unless boarding_pass.size == 10
+
+  row = string_to_num(boarding_pass[...7], 'F', 'B')
+  col = string_to_num(boarding_pass[7...], 'L', 'R')
+  row * 8 + col
+end
+
+# Interpret the string as a binary number, with 1s and 0s being represented by
+# one_char and zero_char, respectively. Returns the number as an Integer.
+def string_to_num(string, zero_char, one_char)
+  raise 'Invalid 0/1 characters' unless (one_char.size == 1) && (zero_char.size == 1)
+  raise '0/1 characters are equal' unless one_char != zero_char
+
+  Integer(string.tr(zero_char + one_char, '01'), 2)
+end
+
+seat_ids = lines.map { |boarding_pass| to_id(boarding_pass) }
+
+p Set.new((seat_ids.min)..(seat_ids.max)) - Set.new(seat_ids)
